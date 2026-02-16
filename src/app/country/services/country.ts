5fcfadb5bdc4c2444e64fd4d83830e2b379b72e4
carlos <carlos.avilam@my.unitec.edu.mx>
2026-02-15 21:22:19 -0600

    se agrega la logoca para manejar las llamadas a nuestro servidor por medio de queryCacheCapital

--- src/app/country/services/country.ts
+++ src/app/country/services/country.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 import { Observable, throwError, of } from 'rxjs';
-import { map, catchError, delay } from 'rxjs/operators';
+import { map, catchError, delay, tap } from 'rxjs/operators';
 import { Country } from '../interfaces/country.interface';
 
 const API_URL = 'https://restcountries.com/v3.1';
@@ -13,14 +13,23 @@ const API_URL = 'https://restcountries.com/v3.1';
 })
 export class CountryService {
   private http = inject(HttpClient);
+  private queryCacheCapital = new Map<string, Country[]>();
 
   searchBYcapital(query: string): Observable<Country[]> {
     query = query.toLowerCase();
+
+
+    if (this.queryCacheCapital.has(query)) {
+      return of(this.queryCacheCapital.get(query)??[]); // Devuelve el resultado cacheado como un Observable
+    }
+
+    console.log(`llegando al servidor por ${query}`); // Solo se ejecutará si no hay un resultado cacheado
 
     return this.http
       .get<RESTCountry[]>(`${API_URL}/capital/${query}`)
       .pipe(
         map((resp) => CountryMapper.mapRestCountryArrayToCountry(resp)),
+        tap((countries) => this.queryCacheCapital.set(query, countries)), // Cachea el resultado
         catchError((error) => {
           console.error('Error en la petición HTTP', error);
           return throwError(() => new Error(`No se pudo buscar el país con ese query:  ${query}`));
